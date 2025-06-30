622928ba36e01497bcf21d9a03f82eb90a1d2cac
Yumeng.Feng <1715302370@qq.com>
2025-06-30 09:54:21 +0800

    feat: 新增控制文本框文本颜色、格式功能，并为程序增加图标

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,10 +7,54 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // 初始化颜色槽函数
+    connect(ui->radioBlack,SIGNAL(clicked()),this, SLOT(SetTextColor()));
+    connect(ui->radioRed,SIGNAL(clicked()),this, SLOT(SetTextColor()));
+    connect(ui->radioBlue,SIGNAL(clicked()),this, SLOT(SetTextColor()));
+    // 初始化字体槽函数
+
+    connect(ui->isUnderLine, SIGNAL(clicked()),this, SLOT(on_chkBoxUnder_clicked()));
+    connect(ui->isItalic, SIGNAL(clicked()),this, SLOT(on_chkItalic_clicked()));
+    connect(ui->isBold, SIGNAL(clicked()),this, SLOT(on_chkBold_clicked()));
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+int MainWindow::SetTextColor()
+{
+
+    QPalette palette = ui->plainTextEdit->palette();
+
+    if(ui->radioBlack->isChecked()) //黑
+        palette.setColor(QPalette::Text,Qt::black);
+    else if (ui->radioRed->isChecked())//红
+        palette.setColor(QPalette::Text,Qt::red);
+    else if(ui->radioBlue->isChecked()) //蓝
+        palette.setColor(QPalette::Text,Qt::blue);
+
+    ui->plainTextEdit->setPalette(palette);
+    return 0;
+}
+
+void MainWindow::on_chkBoxUnder_clicked() {
+    QFont font=ui->plainTextEdit->font();
+    font.setUnderline(ui->isUnderLine->isChecked());
+    ui->plainTextEdit->setFont(font);
+}
+
+void MainWindow::on_chkItalic_clicked() {
+    QFont font=ui->plainTextEdit->font();
+    font.setItalic(ui->isItalic->isChecked());
+    ui->plainTextEdit->setFont(font);
+}
+
+void MainWindow::on_chkBold_clicked() {
+    QFont font=ui->plainTextEdit->font();
+    font.setBold(ui->isBold->isChecked());
+    ui->plainTextEdit->setFont(font);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -21,5 +21,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
+public slots:
+    int     SetTextColor();
+    void    on_chkBoxUnder_clicked();
+    void    on_chkItalic_clicked();
+    void    on_chkBold_clicked();
 };
 #endif // MAINWINDOW_H
